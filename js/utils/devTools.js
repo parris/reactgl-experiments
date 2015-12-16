b7ef334efc41bb63448c7346c6a099026d28ed29
Parris Khachi <parris@letswoosh.com>
2015-12-16 01:19:07 -0800

    Disable devTools being open

--- js/utils/devTools.js
+++ js/utils/devTools.js
@@ -5,7 +5,7 @@ export function renderDevTools(store) {
     let {DevTools, DebugPanel, LogMonitor} = require('redux-devtools/lib/react');
     return (
       <DebugPanel top right bottom>
-        <DevTools store={store} monitor={LogMonitor} />
+        <DevTools store={store} monitor={LogMonitor} visibleOnLoad={false} />
       </DebugPanel>
     );
   }
